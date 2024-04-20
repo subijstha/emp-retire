23636738772d9855048b5b4dbf1355423a6366d7
Subij Shrestha <36372614+subijstha@users.noreply.github.com>
2024-04-20 16:27:19 -0500

    application added

---
+++ employee-retirement/src/main/java/practise/employeeretirement/EmployeeRetirementApplication.java
@@ -0,0 +1,13 @@
+package practise.employeeretirement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeRetirementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeRetirementApplication.class, args);
+	}
+
+}
---
+++ employee-retirement/src/test/java/practise/employeeretirement/EmployeeRetirementApplicationTests.java
@@ -0,0 +1,13 @@
+package practise.employeeretirement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeRetirementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
